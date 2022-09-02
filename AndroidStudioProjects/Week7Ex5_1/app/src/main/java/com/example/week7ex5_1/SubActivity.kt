848d1d2aba758d380d78074ec6f8c28ecc1136fa
yunzae <81746373+yunzae@users.noreply.github.com>
2022-09-02 19:14:45 +0900

    Delete AndroidStudioProjects directory

--- AndroidStudioProjects/Week7Ex5_1/app/src/main/java/com/example/week7ex5_1/SubActivity.kt
+++
@@ -1,4 +0,0 @@
-package com.example.week7ex5_1
-
-class SubActivity {
-}
--- AndroidStudioProjects/week9_2/app/src/main/java/com/example/week9_2/model/PageListModel.kt
+++
@@ -1,6 +0,0 @@
-package com.example.week9_2.model
-
-class PageListModel {
-    var articles: MutableList<ItemModel>? = null
-}
-
